65c41e6f33417821012738a499d1e1bc5dcbb80c
mcmanyika <mcmanyika.com>
2022-10-10 22:31:00 -0500

    static files

--- pro/settings.py
+++ pro/settings.py
@@ -152,10 +152,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static', 'static_dirs'),
+    os.path.join(BASE_DIR, 'static'),
 
 )
 
