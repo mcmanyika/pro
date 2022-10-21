047511fbf7b204fa45e3f4c7b1e4f7252208c16d
mcmanyika <mcmanyika.com>
2022-10-21 15:24:21 -0500

    changed static files

--- pro/settings.py
+++ pro/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static', 'static_dirs'),
