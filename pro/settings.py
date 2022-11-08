4ee8a6d5ef9e872aac5426b39cc51caef068d356
mcmanyika <mcmanyika.com>
2022-11-07 19:51:39 -0600

    debug false

--- pro/settings.py
+++ pro/settings.py
@@ -105,6 +105,10 @@ else:
 
     DATABASES = {
         'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        },
+        'backup': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'bigboy',
             'USER': 'db_admin',
