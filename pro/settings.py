e36916c09f3e2bc7fd03803ad2a58b49901fc71b
mcmanyika <mcmanyika.com>
2022-11-07 19:31:25 -0600

    upload backup

--- pro/settings.py
+++ pro/settings.py
@@ -105,10 +105,6 @@ else:
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        },
-        'backup': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'bigboy',
             'USER': 'db_admin',
